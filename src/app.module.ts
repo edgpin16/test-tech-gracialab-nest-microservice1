cb3d2a5a123aa486997400bef5de872eb1e49c76
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-15 10:40:05 -0400

    Agregando typeorm, mysql2 y realizando la configuracion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersReservationsModule } from './users-reservations/users-reservations.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersReservationsModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    UsersReservationsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: +process.env.PORT,
+      database: process.env.DATABASE_NAME,
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,  
+      retryDelay: 3000,    
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
