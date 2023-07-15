d9690131e7492e97bc13c54ba9449cc445f30e97
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-15 19:29:14 -0400

    Creando los servicios para la creacion de un usuario y reservacion

--- src/users-reservations/users-reservations.service.ts
+++ src/users-reservations/users-reservations.service.ts
@@ -1,15 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
 import { CreateUsersReservationDto } from './dto/create-users-reservation.dto';
 import { UpdateUsersReservationDto } from './dto/update-users-reservation.dto';
 
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { Reservation } from './entities/reservation.entity';
+
 @Injectable()
 export class UsersReservationsService {
-  create(createUsersReservationDto: CreateUsersReservationDto) {
-    return 'This action adds a new usersReservation';
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository : Repository<User>,
+
+    @InjectRepository(Reservation)
+    private readonly reservationRepository : Repository<Reservation>,
+  ){}
+
+  async create(createUsersReservationDto: CreateUsersReservationDto) {
+    try{
+      // Extraemos los campos necesarios ya validados del formulario
+      const {
+        identificacion_document, 
+        name, 
+        last_name, 
+        type_document,
+        email,
+        reservation_date,
+        reservation_type,
+        people_quantity,
+        description,
+        observation,
+      } = createUsersReservationDto;
+
+      //Creamos una nueva instancia tipo entity de users
+      const newUser : User = 
+      this.userRepository.create(
+        {identificacion_document, 
+        type_rol : 2,
+        name, 
+        last_name,
+        type_document, 
+        email,
+        password : null}
+      );
+
+      //Creamos una nueva instancia tipo entity de reservations
+      const newReservation : Reservation =
+      this.reservationRepository.create(
+        {ID_users : identificacion_document,
+        reservation_date, 
+        reservation_type, 
+        people_quantity, 
+        description, 
+        observation, 
+        is_confirm : 0}
+      );
+
+      //Busca si ya existe un usuario registrado por su identificacion
+      const response = await this.userRepository.findOneBy({identificacion_document});
+      
+      //Si no existe
+      if(!response) 
+        await this.userRepository.save(newUser); //Guardamos la instancia en la BD
+      
+      //Si existe, no tiene sentido volver a guardar, solo guardamos la reservacion
+      await this.reservationRepository.save(newReservation);
+
+      return {
+        msg : 'Valores guardados exitosamente',
+        newUser : !response ? newUser : response, //Retorno el nuevo usuario o el usuario encontrado
+        newReservation
+      };
+
+    }
+    catch(error){
+      console.log(error);
+      this.handleDBExceptions(error);
+    }
   }
 
   findAll() {
-    return `This action returns all usersReservations`;
+    return this.userRepository.find({});
+  }
+
+  private handleDBExceptions( error: any ) {
+    if ( error.code === '23505' )
+      throw new BadRequestException(error.detail);
+    
+    throw new InternalServerErrorException('Unexpected error, check server logs', error.message);
   }
 
   // findOne(id: number) {
