54ab2df491b8ee2a00b5e4a7ed70bb4f24e5698a
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-14 23:08:31 -0400

    Agregando el DTO users-reservations

--- src/users-reservations/dto/create-users-reservation.dto.ts
+++ src/users-reservations/dto/create-users-reservation.dto.ts
@@ -1,85 +1,85 @@
-// import { 
-//     IsBoolean,
-//     IsDate, 
-//     IsEnum, 
-//     IsNotEmpty, 
-//     IsNumber, 
-//     IsOptional, 
-//     IsPositive, 
-//     IsString, 
-//     MinLength, 
-//     } 
-// from "class-validator";
+import { 
+    IsBoolean,
+    IsDate, 
+    IsEnum, 
+    IsNotEmpty, 
+    IsNumber, 
+    IsOptional, 
+    IsPositive, 
+    IsString, 
+    MinLength, 
+    } 
+from "class-validator";
 
-// enum typeReservations {
-//     cena = 'cena',
-//     almuerzo = 'almuerzo',
-//     onces = 'onces',
-//     cumpleanos = 'cumplea#os',
-//     ocasion_especial = 'ocasion especial'
-// };
+enum typeReservations {
+    cena = 'cena',
+    almuerzo = 'almuerzo',
+    onces = 'onces',
+    cumpleanos = 'cumplea#os',
+    ocasion_especial = 'ocasion especial'
+};
 
-// export class CreateUsersReservationDto {
+export class CreateUsersReservationDto {
 
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     identificacion_document: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    identificacion_document: string;
 
-//     @IsNumber()
-//     @IsPositive()
-//     @IsOptional()
-//     type_rol?: number;
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    type_rol?: number;
     
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     name: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    name: string;
 
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     last_name: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    last_name: string;
 
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     type_document: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    type_document: string;
 
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     email: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    email: string;
 
-//     @IsString()
-//     @IsOptional()
-//     password: string;
+    @IsString()
+    @IsOptional()
+    password: string;
 
-//     @IsDate()
-//     @IsNotEmpty()
-//     reservation_date : Date;
+    @IsDate()
+    @IsNotEmpty()
+    reservation_date : Date;
 
-//     @IsEnum(typeReservations)
-//     @IsNotEmpty()
-//     reservation_type : string;
+    @IsEnum(typeReservations)
+    @IsNotEmpty()
+    reservation_type : string;
 
-//     @IsNumber()
-//     @IsPositive()
-//     @IsNotEmpty()
-//     people_quantity : number;
+    @IsNumber()
+    @IsPositive()
+    @IsNotEmpty()
+    people_quantity : number;
 
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     description: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    description: string;
 
-//     @IsString()
-//     @MinLength(1)
-//     @IsNotEmpty()
-//     observation: string;
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    observation: string;
 
-//     @IsBoolean()
-//     @IsNotEmpty()
-//     is_confirm: string;
+    @IsBoolean()
+    @IsNotEmpty()
+    is_confirm: string;
 
-// }
+}
