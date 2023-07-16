370db17c13b35cf3df227cc52937e066385a2ca1
Edgpin16 <edgardo.pinto16@gmail.com>
2023-07-16 18:38:42 -0400

    Agregando CORS al proyecto

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,13 @@ async function bootstrap() {
     })
   );
 
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
+
   await app.listen(3000);
 }
 bootstrap();
